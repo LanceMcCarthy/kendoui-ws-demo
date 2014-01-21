349b7ed8073262080db882acb211927569594b6f
Atanas Korchev <akorchev@gmail.com>
2014-01-21 18:02:52 +0200

    WebSocket experiment.

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
 var app = express();
-
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+
+var wss = new WebSocketServer({server: server});
+
+console.log('websocket server created');
+
+wss.on('connection', function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  });
+  }, 1000);
+
+  console.log('websocket connection open');
+
+  ws.on('close', function() {
+    console.log('websocket connection close');
+    clearInterval(id);
+  });
+
 });
