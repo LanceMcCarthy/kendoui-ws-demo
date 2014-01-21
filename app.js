0b68f0e02bc5f8fe4096344b479f09a8d2d475e3
Atanas Korchev <akorchev@gmail.com>
2014-01-21 18:12:01 +0200

    Enable cross domain support (or try to).

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
+});
+
 var server = http.createServer(app);
 
 server.listen(port);
